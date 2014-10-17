cb8ed9b25bfb0ba0296268db21194b96207a3112
muramount <murayama@avelio.co.jp>
2014-10-17 11:39:33 +0900

    共通ヘッダー、フッターファイル追加

---
+++ application/views/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="<?= base_url() ?>/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<script type="text/javascript" src="<?= base_url() ?>/js/jquery-2.1.1.min.js"></script>
+	<script type="text/javascript" src="<?= base_url() ?>/js/bootstrap.min.js"></script>
+  </head>
+  <body>
