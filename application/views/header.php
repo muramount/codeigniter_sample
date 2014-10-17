e4228f8d21f6b744a77f8a26e04a9a6847a862a8
muramount <murayama@avelio.co.jp>
2014-10-17 13:40:21 +0900

    共通ヘッダ、フッタのリファクタリング

--- application/views/header.php
+++ application/views/header.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Bootstrap 101 Template</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="<?= base_url() ?>/css/bootstrap.min.css" rel="stylesheet" media="screen">
-	<script type="text/javascript" src="<?= base_url() ?>/js/jquery-2.1.1.min.js"></script>
-	<script type="text/javascript" src="<?= base_url() ?>/js/bootstrap.min.js"></script>
-  </head>
-  <body>
+	<head>
+		<title><?php echo isset($title) ? $title : $this->uri->uri_string(); ?></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="<?= base_url() ?>/css/bootstrap.min.css" rel="stylesheet" media="screen">
+		<script type="text/javascript" src="<?= base_url() ?>/js/jquery-2.1.1.min.js"></script>
+		<script type="text/javascript" src="<?= base_url() ?>/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="<?= base_url() ?>/js/script.js"></script>
+	</head>
+	<body>
