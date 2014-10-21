caf8c7ea67975cef80cd65e13a1b828533af995f
muramount <murayama@avelio.co.jp>
2014-10-21 17:03:56 +0900

    App_System_Filterのリファクタ

--- application/hooks/App_System_Filter.php
+++ application/hooks/App_System_Filter.php
@@ -1,4 +1,4 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
  * App_System_Filter
@@ -6,33 +6,40 @@
  * System common call back class.
  */
 class App_System_Filter {
-	
+
 	/**
 	 * Called very early during system execution.
 	 */
-	public function pre_system () {
-		log_message('debug', 'Hooks Call to method: ' . __CLASS__ . '::'. __FUNCTION__);
+	public function pre_system() {
+		log_message('debug', 'Hooks Call to method: ' . __CLASS__ . '::' . __FUNCTION__);
 	}
+
+	/**
+	 * Enables you to call your own function instead of the _display_cache() function in the output class.
+	 */
+//	public function cache_override() {
+//		log_message('debug', 'Hooks Call to method: ' . __CLASS__ . '::'. __FUNCTION__);
+//	}
 
 	/**
 	 * Called immediately prior to any of your controllers being called.
 	 */
 	public function pre_controller() {
-		log_message('debug', 'Hooks Call to method: ' . __CLASS__ . '::'. __FUNCTION__);
+		log_message('debug', 'Hooks Call to method: ' . __CLASS__ . '::' . __FUNCTION__);
 	}
 
 	/**
 	 * Called immediately after your controller is instantiated.
 	 */
-	public function post_controller_constructor () {
-		log_message('debug', 'Hooks Call to method: ' . __CLASS__ . '::'. __FUNCTION__);
+	public function post_controller_constructor() {
+		log_message('debug', 'Hooks Call to method: ' . __CLASS__ . '::' . __FUNCTION__);
 	}
 
 	/**
 	 * Called immediately after your controller is fully executed.
 	 */
-	public  function post_controller () {
-		log_message('debug', 'Hooks Call to method: ' . __CLASS__ . '::'. __FUNCTION__);
+	public function post_controller() {
+		log_message('debug', 'Hooks Call to method: ' . __CLASS__ . '::' . __FUNCTION__);
 	}
 
 	/**
@@ -44,18 +51,11 @@ class App_System_Filter {
 //	}
 
 	/**
-	 * Enables you to call your own function instead of the _display_cache() function in the output class.
-	 */
-//	public function cache_override() {
-//		log_message('debug', 'Hooks Call to method: ' . __CLASS__ . '::'. __FUNCTION__);
-//	}
-
-	/**
 	 * Called after the final rendered page is sent to the browser,
 	 * at the end of system execution after the finalized data is sent to the browser.
 	 */
 	public function post_system() {
-		log_message('debug', 'Hooks Call to method: ' . __CLASS__ . '::'. __FUNCTION__);
+		log_message('debug', 'Hooks Call to method: ' . __CLASS__ . '::' . __FUNCTION__);
 	}
 
 }
